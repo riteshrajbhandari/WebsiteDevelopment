b6865db14f3aa5287178dd8e856ecdf6a58580a3
Ritesh Rajbhandari <77riteshrajbhandari77@gmail.com>
2021-07-03 20:29:38 +0545

    update

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -5,3 +5,24 @@ document.getElementById("send").onclick = function () {
 function myFunction() {
 	document.getElementById("sent").innerHTML = "Your message has been sent";
 }
+
+const quotes = [
+	"'You miss 100% of the shots you don’t take. - Wayne Gretzky' - Michael Scott",
+	"I’m an early bird and I’m a night owl so I’m wise and I have worms.",
+	"The worst thing about prison was the Dementors. They were all over the place and they'd suck the soul out of your body and it hurt!",
+	"I love inside jokes. I hope to be a part of one someday.",
+	"You know what they say. Fool me once, strike one, but fool me twice.. .. ...strike three.",
+	"I'm not superstitious, but I am a little stitious.",
+	"I am Beyoncé, always.",
+	"Would I rather be feared or loved? Easy. Both. I want people to be afraid of how much they love me.",
+	"Do I have a special someone? Well, yeah, of course. A bunch of ’em. My employees.",
+	"They say on your deathbed you never wish you spent more time at the office — but I will.",
+	"Make friends first, make sales second, make love third. In no particular order.",
+	"And I’m optimistic because every day I get a little more desperate.",
+	"The people that you work with are, when you get down to it, your very best friends.",
+	"Webster’s Dictionary defines wedding as ‘the fusing of two metals with a hot torch.'",
+	"We’re all homos. Homo… Sapiens.",
+	"It’s never too early for ice cream.",
+	"Don’t ever, for any reason, do anything to anyone, for any reason, ever, no matter what.",
+	"It takes an advanced sense of humor. I don’t expect everyone to understand.",
+];
