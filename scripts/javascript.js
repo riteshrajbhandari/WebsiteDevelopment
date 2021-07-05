06f71ec8c6529d48081c362b82ae0dc1311d6b01
Ritesh Rajbhandari <77riteshrajbhandari77@gmail.com>
2021-07-06 03:29:00 +0545

    update

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -29,27 +29,48 @@ function backToTop() {
 	document.documentElement.scrollTop = 0;
 }
 
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function () {
+	"use strict";
+
+	// Fetch all the forms we want to apply custom Bootstrap validation styles to
+	var forms = document.querySelectorAll(".needs-validation");
+
+	// Loop over them and prevent submission
+	Array.prototype.slice.call(forms).forEach(function (form) {
+		form.addEventListener(
+			"submit",
+			function (event) {
+				if (!form.checkValidity()) {
+					event.preventDefault();
+					event.stopPropagation();
+				}
+
+				form.classList.add("was-validated");
+			},
+			false
+		);
+	});
+})();
+
 function randomize_heading() {
 	let quote_list = [
 		"'You miss 100% of the shots you don’t take. - Wayne Gretzky' - Michael Scott",
 		"I’m an early bird and I’m a night owl so I’m wise and I have worms.",
 		"I love inside jokes. I hope to be a part of one someday.",
-		"You know what they say. Fool me once, strike one, but fool me twice.. .. ...strike three.",
 		"I'm not superstitious, but I am a little stitious.",
 		"I am Beyoncé, always.",
 		"Do I have a special someone? Well, yeah, of course. A bunch of ’em. My employees.",
-		"They say on your deathbed you never wish you spent more time at the office — but I will.",
 		"Make friends first, make sales second, make love third. In no particular order.",
 		"And I’m optimistic because every day I get a little more desperate.",
 		"The people that you work with are, when you get down to it, your very best friends.",
 		"Webster’s Dictionary defines wedding as ‘the fusing of two metals with a hot torch.'",
-		"We’re all homos. Homo… Sapiens.",
 		"It’s never too early for ice cream.",
 		"Don’t ever, for any reason, do anything to anyone, for any reason, ever, no matter what.",
 		"It takes an advanced sense of humor. I don’t expect everyone to understand.",
 	];
 	let quote = quote_list[Math.floor(Math.random() * quote_list.length)];
 	document.getElementById("carousel_text").textContent = quote;
-	setTimeout(randomize_heading, 10000);
+	setTimeout(randomize_heading, 20000);
 }
 randomize_heading();
