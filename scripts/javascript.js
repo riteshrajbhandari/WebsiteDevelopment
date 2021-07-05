987419211b5224438c609ccd531e27745355daa2
Ritesh Rajbhandari <77riteshrajbhandari77@gmail.com>
2021-07-05 17:35:05 +0545

    update

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -6,6 +6,29 @@ function myFunction() {
 	document.getElementById("sent").innerHTML = "Your message has been sent";
 } */
 
+//Get the button
+let mybutton = document.getElementById("btn-back-to-top");
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+	scrollFunction();
+};
+function scrollFunction() {
+	if (
+		document.body.scrollTop > 20 ||
+		document.documentElement.scrollTop > 20
+	) {
+		mybutton.style.display = "block";
+	} else {
+		mybutton.style.display = "none";
+	}
+}
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener("click", backToTop);
+function backToTop() {
+	document.body.scrollTop = 0;
+	document.documentElement.scrollTop = 0;
+}
+
 function randomize_heading() {
 	let quote_list = [
 		"'You miss 100% of the shots you don’t take. - Wayne Gretzky' - Michael Scott",
