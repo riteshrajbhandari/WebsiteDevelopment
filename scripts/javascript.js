5001b1f7e8f1c01105b21d5bceb59018cd8d98b6
Ritesh Rajbhandari <77riteshrajbhandari77@gmail.com>
2021-07-07 03:20:57 +0545

    FINAL-UPDATE

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -19,3 +19,30 @@ function backToTop() {
 	document.body.scrollTop = 0;
 	document.documentElement.scrollTop = 0;
 }
+
+$(function () {
+	AOS.init();
+});
+/*
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function () {
+	var currentScrollPos = window.pageYOffset;
+	if (prevScrollpos > currentScrollPos) {
+		document.getElementById("navbar").style.top = "0";
+	} else {
+		document.getElementById("navbar").style.top = "-50px";
+	}
+	prevScrollpos = currentScrollPos;
+};
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function () {
+	var currentScrollPos = window.pageYOffset;
+	if (prevScrollpos > currentScrollPos) {
+		document.getElementById("navbar").style.top = "0";
+	} else {
+		document.getElementById("navbar").style.top = "-80px";
+	}
+	prevScrollpos = currentScrollPos;
+};
+ */
